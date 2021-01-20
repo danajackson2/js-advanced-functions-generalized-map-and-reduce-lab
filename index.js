77df2a5332efabeee5fd78c2df30cb068a99aa9c
danajackson2 <danajackson2@gmail.com>
2021-01-19 18:08:46 -0800

    WUUUUUT

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your functions here
+function map(arr, func){
+    let new_arr = []
+    arr.forEach(item => {new_arr.push(func(item))})
+    return new_arr
+}
+
+function reduce(sourceArray, func, start){
+    let memo = (!!start) ? start : sourceArray[0]
+    let i = (!!start) ? 0 : 1
+    for (i; i < sourceArray.length; i ++){
+        memo = func(sourceArray[i], memo)
+    }
+    return memo
+}
